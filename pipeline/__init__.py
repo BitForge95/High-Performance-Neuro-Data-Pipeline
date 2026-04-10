bec0aaf091fec4c94c40316df2a377fa1b6082fe
Charan Sai <guttulacharansai@gmail.com>
2026-04-11 00:54:04 +0530

    Fix trailing Hdf5Exporter import statements in __init__after NumpyExporter refactor

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,5 +1,5 @@
 """
-High Performance Neuro data pipeline
+NeuroAlign: High Performance Neuro data pipeline
 """
 
 from .base_loader import BaseNeuroLoader
@@ -7,6 +7,6 @@ from .ephys_loader import EphysLoader
 from .video_loader import VideoLoader
 from .nifti_loader import BidsNiftiLoader
 from .synchronizer import DataSynchronizer
-from .exporter import Hdf5Exporter
+from .exporter import NumpyExporter
 
 __version__ = "0.1.0"
